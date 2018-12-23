3c63726df84dd12f56a117a1711fe091c8bdf75c
Vinamr Loomba Sachdeva <34504513+vinamrsachdeva@users.noreply.github.com>
2018-12-23 10:48:50 +0530

    Create SampleProgram.cpp

---
+++ SampleProgram.cpp
@@ -0,0 +1,22 @@
+/*
+Program No : 00 [Sample Program]
+Developed By : Vinamr L. Sachdeva
+Class Section : XI H
+Date : 20-August-2018
+*/
+
+#include <iostream.h>
+#include <conio.h>
+
+void main(){
+	clrscr();
+
+	cout<<"hello, this is a sample program";
+
+	getch();
+}
+
+/*
+OUTPUT:
+hello, this is a sample program
+*/
