32a712aa5c6c6c6338ea76a1da419daa22ef9548
Vinamr Loomba Sachdeva <34504513+vinamrsachdeva@users.noreply.github.com>
2018-12-23 12:00:00 +0530

    Create L1Q1.cpp

---
+++ L1Q1.cpp
@@ -0,0 +1,38 @@
+/*
+Program No : 01
+Developed By : Vinamr L. Sachdeva
+Class Section : XI H
+Date : 21-July-2018
+*/
+
+#include <iostream.h>
+#include <conio.h>
+
+void main() {
+
+  clrscr();
+
+  float qty, distance;
+
+  cout<<"Enter fuel quantity used = ";
+  cin>>qty;
+  cout<<"Enter distance travelled = ";
+  cin>>distance;
+
+  float avg;
+
+  avg = distance/qty;
+
+  cout<<"The average is = "<<avg<<"Km/L";
+
+  getch();
+}
+
+/*
+OUTPUT:
+Enter quntity of fuel = 10
+
+Enter distance travelled by car = 100
+
+The average is 10Km/L
+*/
