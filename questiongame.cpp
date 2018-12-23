547cba205875f566d1bc61b517e904b0187dd4ae
Vinamr Loomba Sachdeva <34504513+vinamrsachdeva@users.noreply.github.com>
2018-12-23 15:19:25 +0530

    Update questiongame.cpp

--- questiongame.cpp
+++ questiongame.cpp
@@ -1,135 +1,48 @@
 #include <iostream>
-#include <cmath>
+#include <string>
 using namespace std;
 
-//Function prototypes
-float flightrange(float ur, float thetar);
-float hmax(float uh, float thetah);
-float flightduration(float ut, float thetat);
-void position(float uc, float thetac, float tc);
-float launchangle(float range, float ua);
+int readiness();
 
-//Program starts
 int main() {
-	float theta, u;
-	float t;
-	int option;
 
-	//Welcome message :P
-	cout<<"Projectile Calculator v1.3 by Vinamr Sachdeva"<<endl<<"============================================="<<endl;
+  cout<<"Welcome to this Question Game v1.1 by Vinamr Sachdeva"<<endl;
+  cout<<"====================================================="<<endl<<endl;
 
-	//Menu
-	cout<<endl<<"Choose one of the following : "<<endl;
-	cout<<"1. Range"<<endl<<"2. Maximum Height"<<endl<<"3. Flight Duration"<<endl<<"4. Position at a particular time"<<endl<<"5. Range for a given launch angle & launch velocity"<<endl<<endl;
-	cin>>option;
+  if (readiness()==1){
+	string name;
+	cout<<"What is your name ? "<<endl;
+	cin>>name;
 
-	//Conditionals for menu options
+	int age;
+	cout<<"In what year were you born ? "<<endl;
+	cin>>age;
 
-	//Flight Range
-	if (option==1) {
+	string country;
+	cout<<"Where do you live ? "<<endl;
+	cin>>country;
 
-        	cout<<"Enter launch angle (in degrees) : ";
-    		cin>>theta;
-    		theta=theta*3.14/180;
-    		cout<<endl<<"Enter launch velocity : ";
-    		cin>>u;
-
-    		cout<<endl<<endl<<"Range of the Projectle = "<<flightrange(u, theta)<<" meters";
-    		cout<<endl<<"------------"<<endl<<endl;
-	}
-
-	//Maximum height of trajectory
-	else if (option==2) {
-
-		cout<<"Enter launch angle (in degrees) : ";
-    		cin>>theta;
-    		theta=theta*3.14/180;
-    		cout<<endl<<"Enter launch velocity : ";
-    		cin>>u;
-
-    		cout<<endl<<endl<<"Maximum Height of the Projectile = "<<hmax(u, theta)<<" meters";
-    		cout<<endl<<"------------"<<endl<<endl;
-	}
-
-	//Flight Duration
-	else if (option==3) {
-		cout<<"Enter launch angle (in degrees) : ";
-    		cin>>theta;
-    		theta=theta*3.14/180;
-    		cout<<endl<<"Enter launch velocity : ";
-    		cin>>u;
-
-    		cout<<endl<<endl<<"Flight Duration of the Projectle = "<<flightduration(u, theta)<<" seconds";
-    		cout<<endl<<"------------"<<endl<<endl;
-	}
-
-	//Position of object (coordinates)
-	else if (option==4) {
-
-		cout<<"Enter launch angle (in degrees) : ";
-    		cin>>theta;
-    		theta=theta*3.14/180;
-    		cout<<endl<<"Enter launch velocity : ";
-    		cin>>u;
-    		cout<<endl<<"Enter time at which you want coordinates of trajectory : ";
-    		cin>>t;
-
-    		cout<<endl<<endl<<"Position : ";
-    		position(u, theta, t);
-    		cout<<endl<<"------------"<<endl<<endl;
-	}
-
-	else if (option==5) {
-		float range_;
-		cout<<"Enter flight range : ";
-		cin>>range_;
-		cout<<endl<<"Enter launch velocity : ";
-    		cin>>u;
-		cout<<endl<<endl<<"Predicted launch angle : ";
-    		cout<<launchangle(range_,u);
-    		cout<<endl<<"------------"<<endl<<endl;
-	}
-
-	//Error message
-	else {
-	    	cout<<endl<<"You have entered an incorrect input, please enter numbers preceeding one of the following menu options :";
-	    	cout<<endl<<endl<<"Choose one of the following : "<<endl;
-	    	cout<<"1. Range"<<endl<<"2. Maximum Height"<<endl<<"3. Flight Duration"<<"4. Position at a particular time"<<endl<<"5. Range for a given launch angle & launch velocity"<<endl<<endl;
-	    	cin>>option;
-	}
-
+	cout<<endl<<"So, you are "<<name<<", of age "<<(2018-age)<<" from "<<country<<"."<<endl;
+	cout<<endl<<"Thank You for playing, see you next time !";
 	return 0;
-}
+    }
 
-//Functions
+    else if (readiness()==0)
+        cout<<"Ok, I will sit idle. ";
 
-float flightrange(float ur, float thetar){
-    float angle_r=2*thetar;
-    return (ur*ur*sin(angle_r))/9.8;
-}
-
-float hmax(float uh, float thetah){
-    return (uh*uh*sin(thetah)*sin(thetah))/(2*9.8);
-}
-
-float flightduration(float ut, float thetat){
-    return (2*ut*sin(thetat))/9.8;
-}
-
-void position(float uc, float thetac, float tc){
-    float x=uc*cos(thetac)*tc;
-    float y=uc*sin(thetac)*tc-(4.9)*tc*tc;
-    float angle_c=2*thetac;
-
-    if (y>=0)
-    	cout<<"("<<x<<","<<y<<")";
     else
-        cout<<"("<<(uc*uc*sin(angle_c))/9.8<<","<<"0"<<")";
+        cout<<"Invalid, enter 'Y' or 'N'";
 }
 
-float launchangle(float range, float ua){
-    float two_theta=asin((range*9.8)/(ua*ua));
-    float launch_angle = (two_theta/2)*57.2958; //Converting degrees to radians
+    int readiness(){
+        char check;
+        cout<<"Are you ready ? (Y/N)"<<endl;
+        cin>>check;
 
-    return launch_angle;
-}
+        if (check=='Y'||check=='y')
+            return 1;
+        else if (check=='N'||check=='n')
+            return 0;
+        else
+            return 5;
+    }
