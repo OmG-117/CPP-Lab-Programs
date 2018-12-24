c5ac0800365559ff02800650a5447bd726fe57f5
Vinamr Loomba Sachdeva <34504513+vinamrsachdeva@users.noreply.github.com>
2018-12-24 16:31:08 +0530

    Create L1Q6.cpp

---
+++ L1Q6.cpp
@@ -0,0 +1,47 @@
+/*
+Program No : 06
+Developed By : Vinamr L. Sachdeva
+Class Section : XI H
+Date : 20-August-2018
+*/
+
+#include <iostream.h>
+#include <conio.h>
+
+void main() {
+
+  clrscr();
+
+  int basic;
+
+  cout<<"Enter your basic salary : ";
+  cin>>basic;
+
+  int da, hra, it, tsal, salh;
+
+  da = 0.3*basic;
+  hra = 0.25*basic;
+  tsal = basic+da+hra;
+
+  if (basic<50000)
+  {
+    it = 0.1*basic;
+  }
+
+  else
+  {
+    it = 0.2*basic;
+  }
+
+  salh = tsal-it;
+
+  cout<<"Salary in Hand = "<<salh;
+
+  getch();
+}
+
+/*
+OUTPUT:
+Enter your basic salary : 10000
+Salary in Hand = 14499
+*/
