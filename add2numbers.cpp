6d0dca6214db47b55a1fd0a604ef472202b39299
Vinamr Loomba Sachdeva <34504513+vinamrsachdeva@users.noreply.github.com>
2019-01-06 12:12:49 +0530

    Update add2numbers.cpp

--- add2numbers.cpp
+++ add2numbers.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+int main() {
+    
+    int t,a,b;
+    cin>>t;
+    
+    for(int i=1; i<=t; i++)
+    {
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+	
+	return 0;
+}
